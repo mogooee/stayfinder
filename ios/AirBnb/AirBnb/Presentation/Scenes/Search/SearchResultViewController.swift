8104e33820b2becad9d18f56a766fe6b1672a92c
Song TaeHwan <axoghks@gmail.com>
2022-05-27 13:48:15 +0900

    feat: SearchViewController 추가 및 UISearchViewController 연결

---
+++ ios/AirBnb/AirBnb/Presentation/Scenes/Search/SearchResultViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultViewController.swift
+//  AirBnb
+//
+//  Created by 송태환 on 2022/05/26.
+//
+
+import UIKit
+
+class SearchResultViewController: UICollectionViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
