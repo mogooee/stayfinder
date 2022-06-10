b4e2f3882b7a501db035e33faf1ee4429a37e72f
연 <mogoo22@naver.com>
2022-06-10 18:49:48 +0900

    feat: 검색버튼을 클릭하면 지도가 나타난다.

--- fe/src/pages/Search/Search.tsx
+++ fe/src/pages/Search/Search.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import GNB from 'components/GNB/GNB';
+
+const Header = styled.header`
+  height: 94 px;
+`;
+
+const Map = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function Search(): JSX.Element {
-  return <h1>Search</h1>;
+  const { kakao } = window;
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    // eslint-disable-next-line no-unused-vars
+    const map = new kakao.maps.Map(container, options);
+  }, [kakao.maps]);
+
+  return (
+    <main>
+      <Header>
+        <GNB />
+      </Header>
+      <Map id="map" />
+    </main>
+  );
 }
