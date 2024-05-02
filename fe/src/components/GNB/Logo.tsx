33f55538b121c1aa23879fc93abb1c23722316ee
연 <mogoo22@naver.com>
2024-05-02 23:43:29 +0900

    Fix logo href url

--- fe/src/components/GNB/Logo.tsx
+++ fe/src/components/GNB/Logo.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { ReactComponent as LogoIcon } from 'assets/icons/logo.svg';
 
 export default function Logo(): JSX.Element {
-  const ROOT_URL = '/';
   return (
-    <a href={ROOT_URL} className="logo">
+    <a href={process.env.PUBLIC_URL} className="logo">
       <LogoIcon />
     </a>
   );
