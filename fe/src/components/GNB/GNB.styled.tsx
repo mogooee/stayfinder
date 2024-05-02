616da9f6b942ac280c4f7d98c855deeccec73c18
연 <mogoo22@naver.com>
2024-05-02 20:18:34 +0900

    StayFinder 로고 변경

--- fe/src/components/GNB/GNB.styled.tsx
+++ fe/src/components/GNB/GNB.styled.tsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   justify-content: space-around;
   align-items: center;
-  padding: 23px 0;
+  justify-items: center;
+  padding: 23px 40px;
+
+  a {
+    justify-self: start;
+    svg {
+      width: 180px;
+    }
+  }
+
+  button {
+    justify-self: end;
+  }
 `;
